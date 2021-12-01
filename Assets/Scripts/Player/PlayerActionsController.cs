6fd19fa89562a41e4639814891a1c3721e9cd1a4
BiscuitPrime <henri.nomico@gmail.com>
2021-12-01 11:50:30 +0100

    Warrior Action One working

--- Assets/Scripts/Player/PlayerActionsController.cs
+++ Assets/Scripts/Player/PlayerActionsController.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 //It is inherited/interacts with the WarriorController, the ArcherController and the SamouraiController
 public class PlayerActionsController : MonoBehaviour
 {
+    public bool actionOnePossible; //the boolean that will tell wether or not the action one is "possible" i.e. not on cooldown (handled by the inherited scripts)
+
+    private void Start()
+    {
+        actionOnePossible=true;
+    }
+
     // Update is called once per frame
     public void Update()
     {
-        if(Input.GetKey(KeyCode.Mouse0))
+        if(Input.GetKey(KeyCode.Mouse0) && actionOnePossible==true)
         {
             ActionOne();
+            actionOnePossible=false;
         }
         if(Input.GetKey(KeyCode.Mouse1))
         {
--- Assets/Scripts/Player/WarriorController.cs
+++ Assets/Scripts/Player/WarriorController.cs
@@ -8,13 +8,27 @@ public class WarriorController : PlayerActionsController
 {
     private float speed=5f;
     [SerializeField] GameObject swordStrike;
+    [SerializeField] private float cooldownActionOne;
+    private IEnumerator coroutine;
+
+// The Action One
+//--------------------------------------------------------------------------------------------
+    //Calls the coroutine that handles the action One
+    public override void ActionOne()
+    {
+        coroutine=ActionOneCoroutine(cooldownActionOne);
+        StartCoroutine(coroutine);
+    }
 
     //This method is used to instantiate the basic attack
-    public override void ActionOne()
+    private IEnumerator ActionOneCoroutine(float cooldown)
     {
         GameObject new_attack = Instantiate(swordStrike);
         swordStrike.transform.position=transform.position+new Vector3(2,0,0);
+        yield return new WaitForSeconds(cooldown);
+        actionOnePossible=true;
     }
+//--------------------------------------------------------------------------------------------
     public override void ActionTwo(){}
     public override void ActionThree(){}
 }
